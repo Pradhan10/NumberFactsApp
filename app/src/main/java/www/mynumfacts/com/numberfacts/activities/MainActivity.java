36e565c0b641c1cb92be05225c0929f83121adf4
Pradhan10 <pradhanrishi10@gmail.com>
2017-10-12 17:02:08 +0530

    Http request and response working correctly, facts are retrieved and displayed properly. Features to be coded: 1 : Perform code reformatting 2 : append query parameters to request string.

--- app/src/main/java/www/mynumfacts/com/numberfacts/activities/MainActivity.java
+++ app/src/main/java/www/mynumfacts/com/numberfacts/activities/MainActivity.java
@@ -1,21 +1,43 @@
 package www.mynumfacts.com.numberfacts.activities;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import www.mynumfacts.com.numberfacts.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private static final String STRING_REQUEST_URL = "http://numbersapi.com/42";
+    ProgressDialog progressDialog;
     private TextView mTextMessage;
     private Spinner spinner;
     private String category;
-
+    private Button submit;
+    private TextView outputTextView;
+    private TextView mTextFact;
+    private View showDialogView;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -39,28 +61,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(this);
+        submit = (Button) findViewById(R.id.button_submit);
+        mTextFact = (TextView) findViewById(R.id.textView_fact);
+
+
+
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-//        spinner = (Spinner)findViewById(R.id.spinner_category);
+        spinner = (Spinner) findViewById(R.id.spinner);
 
 
-//        List<String> list = new ArrayList<String>();
-//        list.add("list 1");
-//        list.add("list 2");
-//        list.add("list 3");
-//        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,list);
-//        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(dataAdapter);
-//        addListenerOnSpinnerItemSelection();
+        List<String> list = new ArrayList<String>();
+        list.add("trivia");
+        list.add("math");
+        list.add("date");
+        list.add("year");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+        addListenerOnSpinnerItemSelection();
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                volleyStringRequst(STRING_REQUEST_URL);
+            }
+        });
 
     }
 
 
-//    public void addListenerOnSpinnerItemSelection(){
-//
-//        spinner = (Spinner) findViewById(R.id.spinner_category);
-//        spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-//    }
+    public void addListenerOnSpinnerItemSelection() {
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    public void volleyStringRequst(String url) {
+
+        String REQUEST_TAG = "com.androidtutorialpoint.volleyStringRequest";
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        StringRequest strReq = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.d(TAG, response.toString());
+
+
+                LayoutInflater li = LayoutInflater.from(MainActivity.this);
+                showDialogView = li.inflate(R.layout.show_dialog, null);
+                outputTextView = showDialogView.findViewById(R.id.text_view_dialog);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                alertDialogBuilder.setView(showDialogView);
+                alertDialogBuilder
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        })
+                        .setCancelable(false)
+                        .create();
+                outputTextView.setText(response.toString());
+                mTextFact.setText(response.toString());
+                alertDialogBuilder.show();
+                progressDialog.hide();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                progressDialog.hide();
+            }
+        });
+        // Adding String request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq, REQUEST_TAG);
+    }
+
 
 }
